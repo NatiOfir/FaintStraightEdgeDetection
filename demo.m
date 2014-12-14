d5f30d8e78b478449a88f74739ddae60c1519b7a
NatiOfir <natiofir@gmail.com>
2014-12-14 10:42:52 +0200

    demo commit

---
+++ demo.m
@@ -0,0 +1,17 @@
+clc;
+clear all;
+close all;
+%% Real Image
+%I = im2double(imread('images000.tiff'));
+%I = imresize(I,[129 129]); 
+%if ndims(I) == 3
+%    I = rgb2gray(I);
+%end
+
+%% Simulation Image
+I = im2double(imread('Images/Sqr.png'));
+E = runIm(I,0.1);
+m = max(E(:)); E = E./(m+(m==0));
+figure;
+subplot(1,2,1); imshow(I);
+subplot(1,2,2); imshow(E);
